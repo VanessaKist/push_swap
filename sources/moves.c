3a27eda67b7b1e22ff70401b3527baaf15887740
VanessaKist <vanessasilva5666@gmail.com>
2022-10-16 00:20:25 +0200

    pa

---
+++ sources/moves.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   moves.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vkist-si <vkist-si@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/15 19:13:27 by vkist-si          #+#    #+#             */
+/*   Updated: 2022/10/15 20:10:45 by vkist-si         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+"../includes/push_swap.h"
+
+/* static void do_sa(t_element *a)
+{
+	t_element *a;
+	int aux;
+
+	aux = a->content;
+	a->content = a->next;
+	a->next = aux;
+	
+}
+ */
+/* typedef struct s_list
+{
+	void	*content;
+	struct	s_list *next;
+}	t_list;
+
+typedef struct s_element
+{
+	int					content;
+	int					index;
+	int					stack_pos;
+	int					target_pos;
+	int					top_a;
+	int					top_b;
+	struct s_element	*next;
+}	t_element; */
--- sources/push_swap.c
+++ sources/push_swap.c
@@ -6,15 +6,34 @@
 /*   By: vkist-si <vkist-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 18:26:22 by vkist-si          #+#    #+#             */
-/*   Updated: 2022/10/13 23:17:01 by vkist-si         ###   ########.fr       */
+/*   Updated: 2022/10/16 00:03:50 by vkist-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
+void	do_sa(t_element **a)
+{
+	int aux;
+
+	aux = (*a)->content;
+	(*a)->content = (*a)->next->content;
+	(*a)->next->content = aux;
+}
+
+//pb (push B): Take the first element at the top of A and put it at the top of B. Do nothing if A is empty.
+/* void do_pb(t_element **stack, t_element *b)
+{
+	*b = *a;
+	*a = (*a)->next;
+} */
+
 int main (int argc, char **argv)
 {
 	t_element *a;
+	//t_element *b;
+	t_element *aux;
+	
 	int			stack_pos;
 	
 	stack_pos = 0;
@@ -26,9 +45,21 @@ int main (int argc, char **argv)
 		ft_lstadd_back(&a, 
 			ft_lstnew(ft_atoi(*++argv), stack_pos++));
 	ft_get_index(a);
-	while (a)
+	do_sa(&a);
+	/* while (a)
 	{
 		printf("%d:%d - index:%d\n", a->stack_pos, a->content, a->index);
 		a = a->next;
+	}	 */
+
+	// Imprimindo pra verificar
+	aux = a;	
+	puts("\n\n\t\tLISTA DEPOIS DO SWAP");
+	while (aux)
+	{
+		printf("Elemento: %i\n", aux->content);
+		aux = aux->next;
 	}
+	return (0);
 }
+
