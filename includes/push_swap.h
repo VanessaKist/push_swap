92f7967aa57bbd46f541c3851ef4b01613e6db38
VanessaKist <vanessasilva5666@gmail.com>
2022-10-18 01:11:28 +0200

    top_a

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: vkist-si <vkist-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 22:57:52 by vkist-si          #+#    #+#             */
-/*   Updated: 2022/10/13 21:20:15 by vkist-si         ###   ########.fr       */
+/*   Updated: 2022/10/17 21:38:40 by vkist-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,5 @@
 # include "../libraries/42_libft/libft.h"
 
 void	ft_get_index(t_element *a);
-
-typedef struct s_data
-{
-	char	*a;
-	char	*b;
-	char	**stack;
-}	t_data;
 
 #endif
--- sources/push_swap.c
+++ sources/push_swap.c
@@ -6,32 +6,43 @@
 /*   By: vkist-si <vkist-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 18:26:22 by vkist-si          #+#    #+#             */
-/*   Updated: 2022/10/16 00:03:50 by vkist-si         ###   ########.fr       */
+/*   Updated: 2022/10/18 01:08:49 by vkist-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-void	do_sa(t_element **a)
+void	do_sa(t_element **list)
 {
-	int aux;
+	t_element *aux;
 
-	aux = (*a)->content;
-	(*a)->content = (*a)->next->content;
-	(*a)->next->content = aux;
+	aux = (*list)->next;
+	(*list)->next = (*list)->next->next;
+	aux->next = (*list);
+	aux = (*list);
 }
 
 //pb (push B): Take the first element at the top of A and put it at the top of B. Do nothing if A is empty.
-/* void do_pb(t_element **stack, t_element *b)
+ void do_pb(t_element **a, t_element **b)
 {
+	if (!*a)
+		return ;
 	*b = *a;
 	*a = (*a)->next;
+} 
+
+/* void do_ra(t_element **stack)
+{
+	t_element *a;
+	
+	a = (*stack);
+	while (a)
 } */
 
 int main (int argc, char **argv)
 {
 	t_element *a;
-	//t_element *b;
+	t_element *b;
 	t_element *aux;
 	
 	int			stack_pos;
@@ -42,24 +53,28 @@ int main (int argc, char **argv)
 	--argc;
 	a = ft_lstnew(ft_atoi(*++argv), stack_pos++);
 	while (--argc)
+	{
 		ft_lstadd_back(&a, 
 			ft_lstnew(ft_atoi(*++argv), stack_pos++));
+	}
+	a->top_a = ft_atoi(argv[0]);
+	printf("top_a:%d\nstackpos:%d\n", a->top_a, stack_pos);
 	ft_get_index(a);
-	do_sa(&a);
-	/* while (a)
+	//do_sa(&a);
+	//do_pb(&a, &b);
+	//do_ra(&a);
+	while (a)
 	{
 		printf("%d:%d - index:%d\n", a->stack_pos, a->content, a->index);
 		a = a->next;
-	}	 */
-
-	// Imprimindo pra verificar
-	aux = a;	
-	puts("\n\n\t\tLISTA DEPOIS DO SWAP");
-	while (aux)
-	{
-		printf("Elemento: %i\n", aux->content);
-		aux = aux->next;
 	}
-	return (0);
+	// Imprimindo pra verificar
+	/* aux = b;	
+	puts("\n\n\t\tLISTA DEPOIS DO PUSH");
+	
+	printf("Elemento: %i\n", aux->content);
+	//aux = aux->next;
+	
+	return (0); */
 }
 
