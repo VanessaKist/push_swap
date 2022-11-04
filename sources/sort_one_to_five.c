bb1c997f2e3153def02af94e85e14d7f877724d8
vanessakist <vanesasilva_sc@hotmail.com>
2022-11-04 22:38:26 +0100

    agora sim funciona

--- sources/sort_one_to_five.c
+++ sources/sort_one_to_five.c
@@ -6,7 +6,7 @@
 /*   By: vkist-si <vkist-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 23:16:08 by vkist-si          #+#    #+#             */
-/*   Updated: 2022/11/04 03:07:33 by vkist-si         ###   ########.fr       */
+/*   Updated: 2022/11/04 22:34:06 by vkist-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,33 @@
 void	sort_one_to_five(t_element **stack_a, t_element **stack_b)
 {
 	int 	middle_pos;
+	int		to_compare;
 	t_element *last;
 	t_element *aux;
+	to_compare = 0;
+	t_element *aux2;
+
 	//find_position(stack_a);
 	last = ft_lstlast(*stack_a);
 	middle_pos = (last->pos + 1) / 2;
-	int to_compare;
-	to_compare = 0;
+	aux2 = *stack_a;
 	aux = *stack_a;
 	while (to_compare < 2 && aux)
 	{
 		if(aux->index == to_compare)
 		{
-			//printf("aux->index: %d\n", aux->index);
-			if (aux == *stack_a)
-				*stack_a = aux->next;
-			do_push(&aux, stack_b, 'a');
-			to_compare++;
-			aux = *stack_a;
+			while (aux2 != aux)
+			{
+				do_rotate(stack_a);
+				aux2 = *stack_a;
+			}
+		    if (aux == *stack_a)
+		    	*stack_a = aux->next;
+		    do_push(&aux, stack_b, 'a');
+		    to_compare++;
 		}
-		aux = aux->next;
+		else
+			aux = aux->next;
 	}
-	*stack_a = aux;
+	
 }
