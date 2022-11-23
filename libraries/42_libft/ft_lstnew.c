4de8fe200e21b640ac123b2bbbf652a7c74fb4c4
komeno <tkomeno@student.42tokyo.jp>
2022-11-23 19:17:38 -0300

    fixed valgrind error

--- libraries/42_libft/ft_lstnew.c
+++ libraries/42_libft/ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: vkist-si <vkist-si@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/05 20:51:41 by vkist-si          #+#    #+#             */
-/*   Updated: 2022/11/08 22:13:36 by vkist-si         ###   ########.fr       */
+/*   Updated: 2022/11/23 19:16:28 by tkomeno          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_element *ft_lstnew(int content, int stack_pos)
 {
 	t_element *new;
 
-	new = (t_element *)malloc(1 * sizeof(t_element));
+	new = (t_element *)ft_calloc(1, sizeof(t_element));
 	new->content = content;
 	new->stack_pos = stack_pos;
 	new->next = NULL;
